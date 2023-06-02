7b81b4518cc53075f2dec2343df3f51ca1a35fdb
Diksha Dhumal <rudradiksha2073@gmail.com>
2023-06-02 11:30:35 +0530

    Update hashing_dsa.py
    final code hashing

--- hashing_dsa.py
+++ hashing_dsa.py
@@ -1,14 +1,21 @@
+'''
+Consider telephone book database of N clients. Make use of a hash table 
+implementation to quickly look up clients telephone number. Make use of two collision 
+handling techniques and compare them using number of comparisons required to find a 
+set of telephone numbers
+'''
 class Hash_Table:
-    def _init_(self):
+
+    def __init__(self):
         self.m = int(input("Enter size of hash table:"))
 
         if self.m < 3:
             self.prime = self.m
         else:
             prime = [2, 3]
-            for i in range(self.m):
+            for i in range(3,self.m):
+                p=True
                 for j in prime:
-                    p=True
                     if i % j == 0:
                         p = False
                         break
@@ -27,7 +34,7 @@ class Hash_Table:
         self.linkt = [-1] * self.m
         for i in l:
             temp = -1
-            flag = False
+            flag = False #indicator for collision
 
             index = self.hashfunc(i[1])
             if self.hasht[index] != 0:
@@ -49,19 +56,19 @@ class Hash_Table:
     def fill_double(self, l):
         self.doublehashing = [0] * self.m
         for i in l:
-            c = 1
+            c = 1 #no. of collisions
             index = self.hashfunc(i[1])
             while self.doublehashing[index] != 0:
-                index = (self.hashfunc(i[1]) + c * self.hashfunc2(i[1])) % self.m
+                index = (self.hashfunc(i[1]) + c*self.hashfunc2(i[1])) % self.m
                 c += 1
             self.doublehashing[index] = i
 
 
 def check_rep(num):
     for i in entries:
-        if num == i[1]:
-            print("Number already in use")
-            return False
+            if num == i[1]:
+                print("Number already in use")
+                return False
     return True
 
 
